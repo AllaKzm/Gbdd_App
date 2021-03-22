b6ed1c194a49b3c2743fda9f64f6946bf7ddce07
Алла Кузьмина <kad13@mail.ru>
2021-03-22 09:38:02 +0300

    first commit

---
+++ Gbddapp/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gbddapp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void driverBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.driverBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.driverDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "driverDataSet.Driver". При необходимости она может быть перемещена или удалена.
+            this.driverTableAdapter.Fill(this.driverDataSet.Driver);
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Gbdd_App
